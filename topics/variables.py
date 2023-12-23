d07cd6a2b87bb78d2558ef7fdb7b950b619ff111
kc-clintone <kayseeclintone@gmail.com>
2023-12-23 20:35:05 +0000

    mod [40]

--- topics/variables.py
+++ topics/variables.py
@@ -4,8 +4,10 @@
 Let's think of a variable as a container, for example, you want to store a bunch of fruits, you label certain number of containers according to how you want to store the fruits, i.e. you can store the fruits in category of names, colors or even size.
 Picking names as an example, you lebel the containers with different names and store each fruit in a container matching its name. Take a look at this example:
 '''
+
 # a container that holds oranges
 oranges = "a box of oranges"
+
 # a container that holds bananas
 bananas = "a box of bananas"
 apples = "a box of apples"
